48716ccd807e39dd1d6f0e29dcb79e122e33c2a2
d-rajah <114831492+d-rajah@users.noreply.github.com>
2022-12-20 03:17:25 -0500

    Added Producer From Instance

--- kafka-flink-main/example/kafka-demo/producer.py
+++ kafka-flink-main/example/kafka-demo/producer.py
@@ -1,15 +1,19 @@
 import sys
+import tweepy
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
-
 
 def main(argv):
     # kafka broker address
     producer = KafkaProducer(bootstrap_servers=[argv])
-
-    for _ in range(100):
-        # send msg to topic Hello
-        future = producer.send("Hello", b'msg')
+    
+    #Query tweets that contain these topics
+    #Topics were gathered December 17th
+    query = 'Colts OR Matt Ryan OR Ravens OR Lamar OR Greg Roman OR Cubs OR Harbaugh OR P-22 OR Vikes OR Jeff Saturday OR Dansby -is:retweet -has:media -has:images'
+    response = client.search_recent_tweets(query=query, max_results=100,tweet_fields=['created_at', 'lang'])
+    for tweet in response.data:
+        # send msg to topic Tweets
+        future = producer.send("Tweets", bytes(tweet.text,encoding='utf-8'))
         try:
             record_metadata = future.get(timeout=10)
             print(record_metadata)
